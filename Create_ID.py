1b8d452bd5ab9b132c44553a56400b78b9c4fdb7
Diego Manuel <103836783+diegomanuel10x@users.noreply.github.com>
2026-01-11 14:37:16 +0000

    Add files via upload
    This is a simple program that creates a sentence using some of your entered information, this program only uses the def function.

---
+++ Create_ID.py
@@ -0,0 +1,10 @@
+def ID(first_name, second_name):
+    return print(f"ur first name is {first_name}, ur last name is {second_name}")
+
+first = input("name: ")
+second = input("second name: ")
+
+firstie = first
+secondie = second
+
+result = ID(firstie,secondie)

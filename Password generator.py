6dc3fb89b49a75c6f03ee8ccc257ec42c3836038
Diego Manuel <103836783+diegomanuel10x@users.noreply.github.com>
2026-01-27 23:27:28 +0000

    Add files via upload
    A simple password generator!

---
+++ Password generator.py
@@ -0,0 +1,8 @@
+import string
+import secrets
+
+def generate_password(length=12):
+    characters = string.ascii_letters + string.digits + string.punctuation
+    return ''.join(secrets.choice(characters) for _ in range(length))
+
+print(generate_password(16))
